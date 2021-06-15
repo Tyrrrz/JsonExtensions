41b0a249ad3aca6df957ad61a9b77f838017e943
Tyrrrz <tyrrrrrr@gmail.com>
2021-06-15 22:31:40 +0300

    Add coercion reading extension methods

---
+++ JsonExtensions.Tests/Reading/CoercionExtensionsTests.cs
@@ -0,0 +1,442 @@
+﻿using System;
+using FluentAssertions;
+using JsonExtensions.Reading;
+using Xunit;
+
+namespace JsonExtensions.Tests.Reading
+{
+    public class CoercionExtensionsTests
+    {
+        [Fact]
+        public void GetBooleanCoerced_Positive_Direct_Test()
+        {
+            // Arrange
+            var json = Json.Parse("true");
+
+            // Act
+            var value = json.GetBooleanCoerced();
+
+            // Assert
+            value.Should().BeTrue();
+        }
+
+        [Fact]
+        public void GetBooleanCoerced_Positive_FromString_Test()
+        {
+            // Arrange
+            var json = Json.Parse("\"true\"");
+
+            // Act
+            var value = json.GetBooleanCoerced();
+
+            // Assert
+            value.Should().BeTrue();
+        }
+
+        [Fact]
+        public void GetBooleanCoerced_Negative_Test()
+        {
+            // Arrange
+            var json = Json.Parse("null");
+
+            // Act & assert
+            Assert.Throws<InvalidOperationException>(() => json.GetBooleanCoerced());
+        }
+
+        [Fact]
+        public void GetByteCoerced_Positive_Direct_Test()
+        {
+            // Arrange
+            var json = Json.Parse("13");
+
+            // Act
+            var value = json.GetByteCoerced();
+
+            // Assert
+            value.Should().Be(13);
+        }
+
+        [Fact]
+        public void GetByteCoerced_Positive_FromString_Test()
+        {
+            // Arrange
+            var json = Json.Parse("\"13\"");
+
+            // Act
+            var value = json.GetByteCoerced();
+
+            // Assert
+            value.Should().Be(13);
+        }
+
+        [Fact]
+        public void GetByteCoerced_Negative_Test()
+        {
+            // Arrange
+            var json = Json.Parse("null");
+
+            // Act & assert
+            Assert.Throws<InvalidOperationException>(() => json.GetByteCoerced());
+        }
+
+        [Fact]
+        public void GetDecimalCoerced_Positive_Direct_Test()
+        {
+            // Arrange
+            var json = Json.Parse("13");
+
+            // Act
+            var value = json.GetDecimalCoerced();
+
+            // Assert
+            value.Should().Be(13);
+        }
+
+        [Fact]
+        public void GetDecimalCoerced_Positive_FromString_Test()
+        {
+            // Arrange
+            var json = Json.Parse("\"13\"");
+
+            // Act
+            var value = json.GetDecimalCoerced();
+
+            // Assert
+            value.Should().Be(13);
+        }
+
+        [Fact]
+        public void GetDecimalCoerced_Negative_Test()
+        {
+            // Arrange
+            var json = Json.Parse("null");
+
+            // Act & assert
+            Assert.Throws<InvalidOperationException>(() => json.GetDecimalCoerced());
+        }
+
+        [Fact]
+        public void GetDoubleCoerced_Positive_Direct_Test()
+        {
+            // Arrange
+            var json = Json.Parse("13.11");
+
+            // Act
+            var value = json.GetDoubleCoerced();
+
+            // Assert
+            value.Should().Be(13.11);
+        }
+
+        [Fact]
+        public void GetDoubleCoerced_Positive_FromString_Test()
+        {
+            // Arrange
+            var json = Json.Parse("\"13.11\"");
+
+            // Act
+            var value = json.GetDoubleCoerced();
+
+            // Assert
+            value.Should().Be(13.11);
+        }
+
+        [Fact]
+        public void GetDoubleCoerced_Negative_Test()
+        {
+            // Arrange
+            var json = Json.Parse("null");
+
+            // Act & assert
+            Assert.Throws<InvalidOperationException>(() => json.GetDoubleCoerced());
+        }
+
+        [Fact]
+        public void GetSingleCoerced_Positive_Direct_Test()
+        {
+            // Arrange
+            var json = Json.Parse("13.11");
+
+            // Act
+            var value = json.GetSingleCoerced();
+
+            // Assert
+            value.Should().Be(13.11f);
+        }
+
+        [Fact]
+        public void GetSingleCoerced_Positive_FromString_Test()
+        {
+            // Arrange
+            var json = Json.Parse("\"13.11\"");
+
+            // Act
+            var value = json.GetSingleCoerced();
+
+            // Assert
+            value.Should().Be(13.11f);
+        }
+
+        [Fact]
+        public void GetSingleCoerced_Negative_Test()
+        {
+            // Arrange
+            var json = Json.Parse("null");
+
+            // Act & assert
+            Assert.Throws<InvalidOperationException>(() => json.GetSingleCoerced());
+        }
+
+        [Fact]
+        public void GetInt16Coerced_Positive_Direct_Test()
+        {
+            // Arrange
+            var json = Json.Parse("13");
+
+            // Act
+            var value = json.GetInt16Coerced();
+
+            // Assert
+            value.Should().Be(13);
+        }
+
+        [Fact]
+        public void GetInt16Coerced_Positive_FromString_Test()
+        {
+            // Arrange
+            var json = Json.Parse("\"13\"");
+
+            // Act
+            var value = json.GetInt16Coerced();
+
+            // Assert
+            value.Should().Be(13);
+        }
+
+        [Fact]
+        public void GetInt16Coerced_Negative_Test()
+        {
+            // Arrange
+            var json = Json.Parse("null");
+
+            // Act & assert
+            Assert.Throws<InvalidOperationException>(() => json.GetInt16Coerced());
+        }
+
+        [Fact]
+        public void GetInt32Coerced_Positive_Direct_Test()
+        {
+            // Arrange
+            var json = Json.Parse("13");
+
+            // Act
+            var value = json.GetInt32Coerced();
+
+            // Assert
+            value.Should().Be(13);
+        }
+
+        [Fact]
+        public void GetInt32Coerced_Positive_FromString_Test()
+        {
+            // Arrange
+            var json = Json.Parse("\"13\"");
+
+            // Act
+            var value = json.GetInt32Coerced();
+
+            // Assert
+            value.Should().Be(13);
+        }
+
+        [Fact]
+        public void GetInt32Coerced_Negative_Test()
+        {
+            // Arrange
+            var json = Json.Parse("null");
+
+            // Act & assert
+            Assert.Throws<InvalidOperationException>(() => json.GetInt32Coerced());
+        }
+
+        [Fact]
+        public void GetInt64Coerced_Positive_Direct_Test()
+        {
+            // Arrange
+            var json = Json.Parse("13");
+
+            // Act
+            var value = json.GetInt64Coerced();
+
+            // Assert
+            value.Should().Be(13);
+        }
+
+        [Fact]
+        public void GetInt64Coerced_Positive_FromString_Test()
+        {
+            // Arrange
+            var json = Json.Parse("\"13\"");
+
+            // Act
+            var value = json.GetInt64Coerced();
+
+            // Assert
+            value.Should().Be(13);
+        }
+
+        [Fact]
+        public void GetInt64Coerced_Negative_Test()
+        {
+            // Arrange
+            var json = Json.Parse("null");
+
+            // Act & assert
+            Assert.Throws<InvalidOperationException>(() => json.GetInt64Coerced());
+        }
+
+        [Fact]
+        public void GetSByteCoerced_Positive_Direct_Test()
+        {
+            // Arrange
+            var json = Json.Parse("13");
+
+            // Act
+            var value = json.GetSByteCoerced();
+
+            // Assert
+            value.Should().Be(13);
+        }
+
+        [Fact]
+        public void GetSByteCoerced_Positive_FromString_Test()
+        {
+            // Arrange
+            var json = Json.Parse("\"13\"");
+
+            // Act
+            var value = json.GetSByteCoerced();
+
+            // Assert
+            value.Should().Be(13);
+        }
+
+        [Fact]
+        public void GetSByteCoerced_Negative_Test()
+        {
+            // Arrange
+            var json = Json.Parse("null");
+
+            // Act & assert
+            Assert.Throws<InvalidOperationException>(() => json.GetSByteCoerced());
+        }
+
+        [Fact]
+        public void GetUInt16Coerced_Positive_Direct_Test()
+        {
+            // Arrange
+            var json = Json.Parse("13");
+
+            // Act
+            var value = json.GetUInt16Coerced();
+
+            // Assert
+            value.Should().Be(13);
+        }
+
+        [Fact]
+        public void GetUInt16Coerced_Positive_FromString_Test()
+        {
+            // Arrange
+            var json = Json.Parse("\"13\"");
+
+            // Act
+            var value = json.GetUInt16Coerced();
+
+            // Assert
+            value.Should().Be(13);
+        }
+
+        [Fact]
+        public void GetUInt16Coerced_Negative_Test()
+        {
+            // Arrange
+            var json = Json.Parse("null");
+
+            // Act & assert
+            Assert.Throws<InvalidOperationException>(() => json.GetUInt16Coerced());
+        }
+
+        [Fact]
+        public void GetUInt32Coerced_Positive_Direct_Test()
+        {
+            // Arrange
+            var json = Json.Parse("13");
+
+            // Act
+            var value = json.GetUInt32Coerced();
+
+            // Assert
+            value.Should().Be(13);
+        }
+
+        [Fact]
+        public void GetUInt32Coerced_Positive_FromString_Test()
+        {
+            // Arrange
+            var json = Json.Parse("\"13\"");
+
+            // Act
+            var value = json.GetUInt32Coerced();
+
+            // Assert
+            value.Should().Be(13);
+        }
+
+        [Fact]
+        public void GetUInt32Coerced_Negative_Test()
+        {
+            // Arrange
+            var json = Json.Parse("null");
+
+            // Act & assert
+            Assert.Throws<InvalidOperationException>(() => json.GetUInt32Coerced());
+        }
+
+        [Fact]
+        public void GetUInt64Coerced_Positive_Direct_Test()
+        {
+            // Arrange
+            var json = Json.Parse("13");
+
+            // Act
+            var value = json.GetUInt64Coerced();
+
+            // Assert
+            value.Should().Be(13);
+        }
+
+        [Fact]
+        public void GetUInt64Coerced_Positive_FromString_Test()
+        {
+            // Arrange
+            var json = Json.Parse("\"13\"");
+
+            // Act
+            var value = json.GetUInt64Coerced();
+
+            // Assert
+            value.Should().Be(13);
+        }
+
+        [Fact]
+        public void GetUInt64Coerced_Negative_Test()
+        {
+            // Arrange
+            var json = Json.Parse("null");
+
+            // Act & assert
+            Assert.Throws<InvalidOperationException>(() => json.GetUInt64Coerced());
+        }
+    }
+}
---
+++ JsonExtensions/Reading/CoercionExtensions.cs
@@ -0,0 +1,331 @@
+﻿using System;
+using System.Globalization;
+using System.Text.Json;
+
+namespace JsonExtensions.Reading
+{
+    /// <summary>
+    /// Extensions for reading content from <see cref="JsonElement"/> with coercion.
+    /// </summary>
+    public static class CoercionExtensions
+    {
+        /// <summary>
+        /// Gets the value of the element as a <see cref="bool"/>,
+        /// either by reading it directly or by coercing it from a string.
+        ///
+        /// Returns null if the element contains a value of any other kind.
+        /// </summary>
+        public static bool? GetBooleanCoercedOrNull(this JsonElement element) => element.ValueKind switch
+        {
+            JsonValueKind.String => bool.TryParse(element.GetString(), out var result) ? result : null,
+            _ => element.GetBooleanOrNull()
+        };
+
+        /// <summary>
+        /// Gets the value of the element as a <see cref="bool"/>,
+        /// either by reading it directly or by coercing it from a string.
+        /// </summary>
+        public static bool GetBooleanCoerced(this JsonElement element) =>
+            element.GetBooleanCoercedOrNull() ??
+            throw new InvalidOperationException(
+                "Cannot read or coerce the specified JSON element into a boolean value."
+            );
+
+        /// <summary>
+        /// Gets the value of the element as a <see cref="byte"/>,
+        /// either by reading it directly or by coercing it from a string.
+        ///
+        /// Returns null if the element contains a value of any other kind.
+        /// </summary>
+        public static byte? GetByteCoercedOrNull(this JsonElement element) => element.ValueKind switch
+        {
+            JsonValueKind.String => byte.TryParse(
+                element.GetString(),
+                NumberStyles.Integer,
+                CultureInfo.InvariantCulture,
+                out var result) ? result : null,
+
+            _ => element.GetByteOrNull()
+        };
+
+        /// <summary>
+        /// Gets the value of the element as a <see cref="byte"/>,
+        /// either by reading it directly or by coercing it from a string.
+        /// </summary>
+        public static byte GetByteCoerced(this JsonElement element) =>
+            element.GetByteCoercedOrNull() ??
+            throw new InvalidOperationException(
+                "Cannot read or coerce the specified JSON element into a byte value."
+            );
+
+        /// <summary>
+        /// Gets the value of the element as a <see cref="decimal"/>,
+        /// either by reading it directly or by coercing it from a string.
+        ///
+        /// Returns null if the element contains a value of any other kind.
+        /// </summary>
+        public static decimal? GetDecimalCoercedOrNull(this JsonElement element) => element.ValueKind switch
+        {
+            JsonValueKind.String => decimal.TryParse(
+                element.GetString(),
+                NumberStyles.Number,
+                CultureInfo.InvariantCulture,
+                out var result) ? result : null,
+
+            _ => element.GetDecimalOrNull()
+        };
+
+        /// <summary>
+        /// Gets the value of the element as a <see cref="decimal"/>,
+        /// either by reading it directly or by coercing it from a string.
+        /// </summary>
+        public static decimal GetDecimalCoerced(this JsonElement element) =>
+            element.GetDecimalCoercedOrNull() ??
+            throw new InvalidOperationException(
+                "Cannot read or coerce the specified JSON element into a decimal value."
+            );
+
+        /// <summary>
+        /// Gets the value of the element as a <see cref="double"/>,
+        /// either by reading it directly or by coercing it from a string.
+        ///
+        /// Returns null if the element contains a value of any other kind.
+        /// </summary>
+        public static double? GetDoubleCoercedOrNull(this JsonElement element) => element.ValueKind switch
+        {
+            JsonValueKind.String => double.TryParse(
+                element.GetString(),
+                NumberStyles.Number,
+                CultureInfo.InvariantCulture,
+                out var result) ? result : null,
+
+            _ => element.GetDoubleOrNull()
+        };
+
+        /// <summary>
+        /// Gets the value of the element as a <see cref="double"/>,
+        /// either by reading it directly or by coercing it from a string.
+        /// </summary>
+        public static double GetDoubleCoerced(this JsonElement element) =>
+            element.GetDoubleCoercedOrNull() ??
+            throw new InvalidOperationException(
+                "Cannot read or coerce the specified JSON element into a double value."
+            );
+
+        /// <summary>
+        /// Gets the value of the element as a <see cref="float"/>,
+        /// either by reading it directly or by coercing it from a string.
+        ///
+        /// Returns null if the element contains a value of any other kind.
+        /// </summary>
+        public static float? GetSingleCoercedOrNull(this JsonElement element) => element.ValueKind switch
+        {
+            JsonValueKind.String => float.TryParse(
+                element.GetString(),
+                NumberStyles.Number,
+                CultureInfo.InvariantCulture,
+                out var result) ? result : null,
+
+            _ => element.GetSingleOrNull()
+        };
+
+        /// <summary>
+        /// Gets the value of the element as a <see cref="float"/>,
+        /// either by reading it directly or by coercing it from a string.
+        /// </summary>
+        public static float GetSingleCoerced(this JsonElement element) =>
+            element.GetSingleCoercedOrNull() ??
+            throw new InvalidOperationException(
+                "Cannot read or coerce the specified JSON element into a float value."
+            );
+
+        /// <summary>
+        /// Gets the value of the element as a <see cref="short"/>,
+        /// either by reading it directly or by coercing it from a string.
+        ///
+        /// Returns null if the element contains a value of any other kind.
+        /// </summary>
+        public static short? GetInt16CoercedOrNull(this JsonElement element) => element.ValueKind switch
+        {
+            JsonValueKind.String => short.TryParse(
+                element.GetString(),
+                NumberStyles.Integer,
+                CultureInfo.InvariantCulture,
+                out var result) ? result : null,
+
+            _ => element.GetInt16OrNull()
+        };
+
+        /// <summary>
+        /// Gets the value of the element as a <see cref="short"/>,
+        /// either by reading it directly or by coercing it from a string.
+        /// </summary>
+        public static short GetInt16Coerced(this JsonElement element) =>
+            element.GetInt16CoercedOrNull() ??
+            throw new InvalidOperationException(
+                "Cannot read or coerce the specified JSON element into a short value."
+            );
+
+        /// <summary>
+        /// Gets the value of the element as an <see cref="int"/>,
+        /// either by reading it directly or by coercing it from a string.
+        ///
+        /// Returns null if the element contains a value of any other kind.
+        /// </summary>
+        public static int? GetInt32CoercedOrNull(this JsonElement element) => element.ValueKind switch
+        {
+            JsonValueKind.String => int.TryParse(
+                element.GetString(),
+                NumberStyles.Integer,
+                CultureInfo.InvariantCulture,
+                out var result) ? result : null,
+
+            _ => element.GetInt32OrNull()
+        };
+
+        /// <summary>
+        /// Gets the value of the element as an <see cref="int"/>,
+        /// either by reading it directly or by coercing it from a string.
+        /// </summary>
+        public static int GetInt32Coerced(this JsonElement element) =>
+            element.GetInt32CoercedOrNull() ??
+            throw new InvalidOperationException(
+                "Cannot read or coerce the specified JSON element into an int value."
+            );
+
+        /// <summary>
+        /// Gets the value of the element as a <see cref="long"/>,
+        /// either by reading it directly or by coercing it from a string.
+        ///
+        /// Returns null if the element contains a value of any other kind.
+        /// </summary>
+        public static long? GetInt64CoercedOrNull(this JsonElement element) => element.ValueKind switch
+        {
+            JsonValueKind.String => long.TryParse(
+                element.GetString(),
+                NumberStyles.Integer,
+                CultureInfo.InvariantCulture,
+                out var result) ? result : null,
+
+            _ => element.GetInt64OrNull()
+        };
+
+        /// <summary>
+        /// Gets the value of the element as a <see cref="long"/>,
+        /// either by reading it directly or by coercing it from a string.
+        /// </summary>
+        public static long GetInt64Coerced(this JsonElement element) =>
+            element.GetInt64CoercedOrNull() ??
+            throw new InvalidOperationException(
+                "Cannot read or coerce the specified JSON element into a long value."
+            );
+
+        /// <summary>
+        /// Gets the value of the element as an <see cref="sbyte"/>,
+        /// either by reading it directly or by coercing it from a string.
+        ///
+        /// Returns null if the element contains a value of any other kind.
+        /// </summary>
+        public static sbyte? GetSByteCoercedOrNull(this JsonElement element) => element.ValueKind switch
+        {
+            JsonValueKind.String => sbyte.TryParse(
+                element.GetString(),
+                NumberStyles.Integer,
+                CultureInfo.InvariantCulture,
+                out var result) ? result : null,
+
+            _ => element.GetSByteOrNull()
+        };
+
+        /// <summary>
+        /// Gets the value of the element as an <see cref="sbyte"/>,
+        /// either by reading it directly or by coercing it from a string.
+        /// </summary>
+        public static sbyte GetSByteCoerced(this JsonElement element) =>
+            element.GetSByteCoercedOrNull() ??
+            throw new InvalidOperationException(
+                "Cannot read or coerce the specified JSON element into an sbyte value."
+            );
+
+        /// <summary>
+        /// Gets the value of the element as a <see cref="ushort"/>,
+        /// either by reading it directly or by coercing it from a string.
+        ///
+        /// Returns null if the element contains a value of any other kind.
+        /// </summary>
+        public static ushort? GetUInt16CoercedOrNull(this JsonElement element) => element.ValueKind switch
+        {
+            JsonValueKind.String => ushort.TryParse(
+                element.GetString(),
+                NumberStyles.Integer,
+                CultureInfo.InvariantCulture,
+                out var result) ? result : null,
+
+            _ => element.GetUInt16OrNull()
+        };
+
+        /// <summary>
+        /// Gets the value of the element as a <see cref="ushort"/>,
+        /// either by reading it directly or by coercing it from a string.
+        /// </summary>
+        public static ushort GetUInt16Coerced(this JsonElement element) =>
+            element.GetUInt16CoercedOrNull() ??
+            throw new InvalidOperationException(
+                "Cannot read or coerce the specified JSON element into a ushort value."
+            );
+
+        /// <summary>
+        /// Gets the value of the element as a <see cref="uint"/>,
+        /// either by reading it directly or by coercing it from a string.
+        ///
+        /// Returns null if the element contains a value of any other kind.
+        /// </summary>
+        public static uint? GetUInt32CoercedOrNull(this JsonElement element) => element.ValueKind switch
+        {
+            JsonValueKind.String => uint.TryParse(
+                element.GetString(),
+                NumberStyles.Integer,
+                CultureInfo.InvariantCulture,
+                out var result) ? result : null,
+
+            _ => element.GetUInt32OrNull()
+        };
+
+        /// <summary>
+        /// Gets the value of the element as a <see cref="uint"/>,
+        /// either by reading it directly or by coercing it from a string.
+        /// </summary>
+        public static uint GetUInt32Coerced(this JsonElement element) =>
+            element.GetUInt32CoercedOrNull() ??
+            throw new InvalidOperationException(
+                "Cannot read or coerce the specified JSON element into a uint value."
+            );
+
+        /// <summary>
+        /// Gets the value of the element as a <see cref="ulong"/>,
+        /// either by reading it directly or by coercing it from a string.
+        ///
+        /// Returns null if the element contains a value of any other kind.
+        /// </summary>
+        public static ulong? GetUInt64CoercedOrNull(this JsonElement element) => element.ValueKind switch
+        {
+            JsonValueKind.String => ulong.TryParse(
+                element.GetString(),
+                NumberStyles.Integer,
+                CultureInfo.InvariantCulture,
+                out var result) ? result : null,
+
+            _ => element.GetUInt64OrNull()
+        };
+
+        /// <summary>
+        /// Gets the value of the element as a <see cref="ulong"/>,
+        /// either by reading it directly or by coercing it from a string.
+        /// </summary>
+        public static ulong GetUInt64Coerced(this JsonElement element) =>
+            element.GetUInt64CoercedOrNull() ??
+            throw new InvalidOperationException(
+                "Cannot read or coerce the specified JSON element into a ulong value."
+            );
+    }
+}
